ed8b5dbfe278ce8c0cdcfa956c9736edebdda16c
ivankp <ivan.pogrebnyak@gmail.com>
2026-01-11 16:02:42 -0500

    addition, subtraction, multiplication, division

--- include/Units.hpp
+++ include/Units.hpp
@@ -2,14 +2,30 @@
 
 #include <cstdint>
 #include <cmath>
+#include <type_traits>
 
 namespace units {
 
-template <int8_t kg, int8_t m, int8_t s, typename T>
+template <typename T, int8_t kg, int8_t m, int8_t s>
+class Quantity;
+
+template <typename T>
+struct IsQuantity : std::false_type { };
+
+template <typename T, int8_t kg, int8_t m, int8_t s>
+struct IsQuantity<Quantity<T, kg, m, s>> : std::true_type { };
+
+template <typename T>
+concept AQuantity = IsQuantity<T>::value;
+
+template <typename T>
+concept NotAQuantity = !IsQuantity<T>::value;
+
+template <typename T, int8_t kg, int8_t m, int8_t s>
 class Quantity {
     T value;
 
-    template <int8_t, int8_t, int8_t, typename>
+    template <typename, int8_t, int8_t, int8_t>
     friend class Quantity;
 
 public:
@@ -22,33 +38,115 @@ public:
         return value;
     }
 
-    constexpr Quantity operator+(Quantity o) const noexcept {
-        return value + o.value;
+    // Addition ----------------------------------------------------------------
+
+    template <typename R>
+    friend constexpr auto operator+(Quantity l, Quantity<R, kg, m, s> r) noexcept
+    -> Quantity<decltype(l.value + r.value), kg, m, s>
+    {
+        return l.value + r.value;
     }
 
-    constexpr Quantity operator-(Quantity o) const noexcept {
-        return value - o.value;
+    template <typename R>
+    constexpr Quantity& operator+=(Quantity<R, kg, m, s> r) noexcept {
+        value += r.value;
+        return *this;
     }
 
-    template <int8_t Rkg, int8_t Rm, int8_t Rs, typename RT>
-    constexpr Quantity<kg + Rkg, m + Rm, s + Rs, decltype(T{} * RT{})>
-    operator*(Quantity<Rkg, Rm, Rs, RT> o) const noexcept {
-        return value * o.value;
+    // Subtraction -------------------------------------------------------------
+
+    template <typename R>
+    friend constexpr auto operator-(Quantity l, Quantity<R, kg, m, s> r) noexcept
+    -> Quantity<decltype(l.value - r.value), kg, m, s>
+    {
+        return l.value - r.value;
     }
 
-    template <int8_t Rkg, int8_t Rm, int8_t Rs, typename RT>
-    constexpr Quantity<kg - Rkg, m - Rm, s - Rs, decltype(T{} / RT{})>
-    operator/(Quantity<Rkg, Rm, Rs, RT> o) const noexcept {
-        return value / o.value;
+    template <typename R>
+    constexpr Quantity& operator-=(Quantity<R, kg, m, s> r) noexcept {
+        value -= r.value;
+        return *this;
     }
+
+    // Multiplication ----------------------------------------------------------
+
+    template <typename R, int8_t Rkg, int8_t Rm, int8_t Rs>
+    friend constexpr auto operator*(Quantity l, Quantity<R, Rkg, Rm, Rs> r) noexcept
+    -> Quantity<decltype(l.value * r.value), kg + Rkg, m + Rm, s + Rs>
+    {
+        return l.value * r.value;
+    }
+
+    friend constexpr auto operator*(Quantity l, NotAQuantity auto r) noexcept
+    -> Quantity<decltype(l.value * r), kg, m, s>
+    {
+        return l.value * r;
+    }
+
+    friend constexpr auto operator*(NotAQuantity auto l, Quantity r) noexcept
+    -> Quantity<decltype(l * r.value), kg, m, s>
+    {
+        return l * r.value;
+    }
+
+    constexpr Quantity& operator*=(auto r) noexcept {
+        value *= r.value;
+        return *this;
+    }
+
+    // Division ----------------------------------------------------------------
+
+    template <typename R, int8_t Rkg, int8_t Rm, int8_t Rs>
+    friend constexpr auto operator/(Quantity l, Quantity<R, Rkg, Rm, Rs> r) noexcept
+    -> Quantity<decltype(l.value / r.value), kg - Rkg, m - Rm, s - Rs>
+    {
+        return l.value / r.value;
+    }
+
+    friend constexpr auto operator/(Quantity l, NotAQuantity auto r) noexcept
+    -> Quantity<decltype(l.value / r), kg, m, s>
+    {
+        return l.value / r;
+    }
+
+    friend constexpr auto operator/(NotAQuantity auto l, Quantity r) noexcept
+    -> Quantity<decltype(l / r.value), kg, m, s>
+    {
+        return l / r.value;
+    }
+
+    constexpr Quantity& operator/=(auto r) noexcept {
+        value /= r.value;
+        return *this;
+    }
+
+    // Exponentiation ----------------------------------------------------------
 
     template <int8_t n>
-    friend constexpr Quantity<kg*n, m*n, s*n, T> pow(Quantity q) noexcept {
+    friend constexpr Quantity<T, kg*n, m*n, s*n> pow(Quantity q) noexcept {
         return std::pow(q.value, n);
     }
 };
 
 template <typename T>
-Quantity(T x) -> Quantity<0, 0, 0, T>;
+Quantity(T x) -> Quantity<T, 0, 0, 0>;
+
+template <typename T>
+using Dimensionless = Quantity<T, 0, 0, 0>;
+
+template <typename T>
+using Mass = Quantity<T, 1, 0, 0>;
+
+template <typename T>
+using Length = Quantity<T, 0, 1, 0>;
+
+template <typename T>
+using Time = Quantity<T, 0, 0, 1>;
+
+template <typename T>
+using Speed = Quantity<T, 0, 1, -1>;
+
+template <typename T>
+using Energy = Quantity<T, 1, 2, -2>;
 
 } // namespace units
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,11 +4,11 @@
 
 #include <type_traits>
 
-using units::Quantity;
+using namespace units;
 
 TEST(Size) {
-    static_assert(sizeof(Quantity<0,1,0,double>) == sizeof(double));
-    static_assert(sizeof(Quantity<0,1,0,float >) == sizeof(float ));
+    static_assert(sizeof(Quantity<double,0,1,0>) == sizeof(double));
+    static_assert(sizeof(Quantity<float ,0,1,0>) == sizeof(float ));
 }
 
 TEST(ImplicitConversion) {
@@ -17,32 +17,73 @@ TEST(ImplicitConversion) {
     TEST_EQ(value, 5.);
 }
 
-// TEST(Meter) {
-//     Quantity<0,1,0> a = 5;
-//     double b = a;
-//     TEST_EQ(b, 5.);
-// }
+TEST(Addition) {
+    Quantity<double,1,0,0> a = 5.;
+    Quantity<float,1,0,0> b = 2.;
+    auto c = a + b;
+    static_assert(std::is_same_v<decltype(c), Quantity<double,1,0,0>>);
+}
+
+TEST(Addition2) {
+    Quantity<double,1,0,0> a = 5.;
+    Quantity<float,1,0,0> b = 2.;
+    a += b;
+}
+
+TEST(Subtraction) {
+    Quantity<double,1,0,0> a = 5.;
+    Quantity<float,1,0,0> b = 2.;
+    auto c = a - b;
+    static_assert(std::is_same_v<decltype(c), Quantity<double,1,0,0>>);
+}
+
+TEST(Subtraction2) {
+    Quantity<double,1,0,0> a = 5.;
+    Quantity<float,1,0,0> b = 2.;
+    a -= b;
+}
 
 TEST(Multiplication) {
-    Quantity<0,1,0,double> length = 5.;
-    Quantity<0,0,1,float> time = 2.;
+    Quantity<double,0,1,0> length = 5.;
+    Quantity<float,0,0,1> time = 2.;
     auto product = length * time;
-    static_assert(std::is_same_v<decltype(product), Quantity<0,1,1,double>>);
+    static_assert(std::is_same_v<decltype(product), Quantity<double,0,1,1>>);
 }
 
 TEST(Division) {
-    Quantity<0,1,0,double> length = 5.;
-    Quantity<0,0,1,float> time = 2.f;
+    Quantity<double,0,1,0> length = 5.;
+    Quantity<float,0,0,1> time = 2.f;
     auto speed = length / time;
-    static_assert(std::is_same_v<decltype(speed), Quantity<0,1,-1,double>>);
-    double value = speed / Quantity<0,1,-1,int>(1);
+    static_assert(std::is_same_v<decltype(speed), Quantity<double,0,1,-1>>);
+    double value = speed / Quantity<int,0,1,-1>(1);
     TEST_EQ(value, 2.5);
 }
 
-TEST(Power) {
-    Quantity<0,1,0,float> length = 5.f;
+TEST(MultiplicationByFactor) {
+    Length<double> l1 = 5;
+    auto l2 = 2. * l1;
+    static_assert(std::is_same_v<decltype(l2), Length<double>>);
+    auto l3 = l1 * 3.;
+    static_assert(std::is_same_v<decltype(l3), Length<double>>);
+    TEST_EQ(double(l2 / l1), 2);
+    TEST_EQ(double(l3 / l1), 3);
+}
+
+TEST(Exponentiation) {
+    Quantity<float,0,1,0> length = 5.f;
     auto area = pow<2>(length);
-    static_assert(std::is_same_v<decltype(area), Quantity<0,2,0,float>>);
-    double value = area / Quantity<0,2,0,int>(1);
+    static_assert(std::is_same_v<decltype(area), Quantity<float,0,2,0>>);
+    double value = area / Quantity<int,0,2,0>(1);
     TEST_EQ(value, 25);
+}
+
+TEST(CompoundQuantity) {
+    Mass<double> m = 7;
+    Length<double> l = 3;
+    Time<double> t = 5;
+    auto v = l/t;
+    static_assert(std::is_same_v<decltype(v), Speed<double>>);
+    [](Energy<double> E){
+        TEST_EQ(double(E / Energy<double>(1)), 2.52);
+    }(m * pow<2>(v));
 }
